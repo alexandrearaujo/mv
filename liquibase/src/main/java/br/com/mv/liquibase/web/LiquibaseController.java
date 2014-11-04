16bd374a635167043081889bbda0726ec4a02911
alexandrearaujo <alexandre.garaujo@outlook.com>
2014-11-04 16:53:31 -0300

    alterações

--- liquibase/src/main/java/br/com/mv/liquibase/web/LiquibaseController.java
+++ liquibase/src/main/java/br/com/mv/liquibase/web/LiquibaseController.java
@@ -1,12 +1,17 @@
 package br.com.mv.liquibase.web;
 
 import java.sql.SQLException;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
 
 import liquibase.exception.LiquibaseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.mv.liquibase.service.LiquibaseDatabaseService;
 import br.com.mv.liquibase.service.ParametroService;
@@ -37,4 +42,14 @@ public class LiquibaseController {
 		liquibaseDatabaseService.testeConexao();
 		return "index";
 	}	
+	
+	@ExceptionHandler(Exception.class)
+    public ModelAndView handleException(HttpServletRequest req, Exception e) {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", e);
+        mav.addObject("timestamp", new Date());
+        mav.addObject("url", req.getRequestURL());
+        mav.setViewName("exception");
+        return mav;
+    }
 }
